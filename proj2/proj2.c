09728a7263b97e4a9403ff3023cc9725daa0f539
xstrei06 <xstrei06@stud.fit.vutbr.cz>
2022-04-23 13:53:36 +0200

    TODO find deadlock in ox_queue

--- proj2/proj2.c
+++ proj2/proj2.c
@@ -16,14 +16,15 @@
 
 //semaphores
 sem_t *mutex;
-sem_t *barrier;
+sem_t *barrier1;
+sem_t *barrier2;
 sem_t *oxyQueue;
 sem_t *hydroQueue;
 sem_t *mutex2;
 
 FILE *file;
 
-void sems_init(sem_t **mutex, sem_t **oxyQueue, sem_t **hydroQueue, sem_t **barrier, sem_t **mutex2){
+void sems_init(sem_t **mutex, sem_t **oxyQueue, sem_t **hydroQueue, sem_t **barrier1, sem_t **barrier2, sem_t **mutex2){
     *mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     sem_init(*mutex, 1, 1);
 
@@ -33,14 +34,16 @@ void sems_init(sem_t **mutex, sem_t **oxyQueue, sem_t **hydroQueue, sem_t **barr
     *hydroQueue = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     sem_init(*hydroQueue, 1, 0);
 
-    *barrier = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    sem_init(*barrier, 1, 3);
+    *barrier1 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(*barrier1, 1, 0);
+    *barrier2 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    sem_init(*barrier2, 1, 0);
 
     *mutex2 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     sem_init(*mutex2, 1, 1);
 }
 
-void ox_code(int *oxygen, int *hydrogen, int *line_num, int idO, int TI, int *noM){
+void ox_queue(int *oxygen, int *hydrogen, int *line_num, int idO, int TI, int *noM, int *count){
     sem_wait(mutex);
     //fprintf(file, "%d: O %d: started", *line_num, idO);
     printf("%d: O %d: started\n", *line_num, idO);
@@ -62,19 +65,38 @@ void ox_code(int *oxygen, int *hydrogen, int *line_num, int idO, int TI, int *no
     else{
         sem_post(mutex);
     }
+    return;
+
     sem_wait(oxyQueue);
+
+    //return;
+
     sem_wait(mutex2);
-    printf("%d: H %d: creating molecule %d", *line_num, idO, *noM);
+    printf("%d: O %d: creating molecule %d\n", *line_num, idO, *noM);
+    *line_num += 1;
+    *count += 1;
+    sem_post(mutex2);
+
+    if (*count == 3){
+        sem_post(barrier1);
+        sem_post(barrier1);
+        *count = 0;
+    }
+    else{
+        sem_wait(barrier1);
+    }
+
+    sem_wait(mutex2);
+    printf("%d: O %d: molecule %d created\n", *line_num, idO, *noM);
     *line_num += 1;
     sem_post(mutex2);
 
-    //TODO bond();
 
-    sem_wait(barrier);
+    //sem_wait(barrier1);
     sem_post(mutex);
 }
 
-void hyd_code(int *oxygen, int *hydrogen, int *line_num, const int idH, int TI, int *noM){
+void hyd_queue(int *oxygen, int *hydrogen, int *line_num, const int idH, int TI, int *noM, int *count){
     sem_wait(mutex);
     //fprintf(file, "%d: H %d: started", *line_num, *idH);
     printf("%d: H %d: started\n", *line_num, idH);
@@ -96,15 +118,33 @@ void hyd_code(int *oxygen, int *hydrogen, int *line_num, const int idH, int TI, 
     else{
         sem_post(mutex);
     }
+
     sem_wait(hydroQueue);
+
     sem_wait(mutex2);
-    printf("%d: H %d: creating molecule %d", *line_num, idH, *noM);
+    printf("%d: H %d: creating molecule %d\n", *line_num, idH, *noM);
+    *line_num += 1;
+    *count += 1;
+    sem_post(mutex2);
+
+    printf("count: %d", *count);
+    if (*count == 3){
+        sem_post(barrier1);
+        sem_post(barrier1);
+        *count = 0;
+    }
+    else{
+        sem_wait(barrier1);
+    }
+
+    sem_wait(mutex2);
+    printf("%d: H %d: molecule %d created\n", *line_num, idH, *noM);
     *line_num += 1;
     sem_post(mutex2);
 
-    //TODO bond();
 
-    sem_wait(barrier);
+    //sem_wait(barrier2);
+
 }
 
 int check_args(int argc, char *argv[], long *NO, long *NH, long *TI, long *TB){
@@ -172,7 +212,10 @@ int main(int argc, char *argv[]) {
     *line_num = 1;
 
     int *noM = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    *noM = 0;
+    *noM = 1;
+
+    int *count = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *count = 0;
 
     int err = check_args(argc, argv, &NO, &NH, &TI, &TB);
     if(err){
@@ -184,15 +227,13 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    sems_init(&mutex, &oxyQueue, &hydroQueue, &barrier, &mutex2);
+    sems_init(&mutex, &oxyQueue, &hydroQueue, &barrier1, &barrier2, &mutex2);
 
     for (int i = 1; i <= NO; i++){
         idO++;
         pid_t id = fork();
         if(id == 0) {
-            ox_code(oxygen, hydrogen, line_num, idO, TI, noM);
-            //printf("picovina toto nechapem tomu %d\n", getpid());
-            //printf("%d\n", *line_num);
+            ox_queue(oxygen, hydrogen, line_num, idO, TI, noM, count);
             exit(0);
         }
     }
@@ -202,8 +243,7 @@ int main(int argc, char *argv[]) {
         idH++;
         pid_t id = fork();
         if(id == 0) {
-            hyd_code(oxygen, hydrogen, line_num, idH, TI, noM);
-            //printf("hento vodiky prijebane tu kokotiny robi %d\n", getpid());
+            hyd_queue(oxygen, hydrogen, line_num, idH, TI, noM, count);
             exit(0);
         }
     }
